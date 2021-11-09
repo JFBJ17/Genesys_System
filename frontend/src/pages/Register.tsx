83c4a4b7708c162c2078202c8da5e1dc715752d5
t4rtufo <walt2001@icloud.com>
2021-11-09 11:44:39 -0500

    loader agregado

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,49 +5,63 @@ import '../assets/styles/register.scss';
 
 const Register = () => {
   return (
-    <div className='register-container'>
-      <div className='register'>
-        <h2>Registrarse</h2>
-        <form action=''>
-          <div className='input-box'>
-            <AiOutlineCheckCircle className='icon' color='#818181' />
-            <input
-              className='input-field'
-              type='text'
-              placeholder='Nombre'
-            />
-          </div>
-          <div className='input-box'>
-            <AiOutlineCheckCircle className='icon' color='#818181' />
-            <input
-              className='input-field'
-              type='email'
-              placeholder='Email'
-            />
-          </div>
-          <div className='input-box'>
-            <AiOutlineCheckCircle className='icon' color='#818181' />
-            <input
-              className='input-field'
-              type='password'
-              placeholder='Contraseña'
-            />
-          </div>
-          <div className='input-box'>
-            <AiOutlineCheckCircle className='icon' color='#818181' />
-            <input
-              className='input-field'
-              type='password'
-              placeholder='Repetir Contraseña'
-            />
-          </div>
+    <div className='align-box'>
+      <div className='register-container'>
+        <div className='register'>
+          <h2>Registrarse</h2>
+          <form action=''>
+            <div className='input-box'>
+              <AiOutlineCheckCircle
+                className='icon'
+                color='#818181'
+              />
+              <input
+                className='input-field'
+                type='text'
+                placeholder='Nombre'
+              />
+            </div>
+            <div className='input-box'>
+              <AiOutlineCheckCircle
+                className='icon'
+                color='#818181'
+              />
+              <input
+                className='input-field'
+                type='email'
+                placeholder='Email'
+              />
+            </div>
+            <div className='input-box'>
+              <AiOutlineCheckCircle
+                className='icon'
+                color='#818181'
+              />
+              <input
+                className='input-field'
+                type='password'
+                placeholder='Contraseña'
+              />
+            </div>
+            <div className='input-box'>
+              <AiOutlineCheckCircle
+                className='icon'
+                color='#818181'
+              />
+              <input
+                className='input-field'
+                type='password'
+                placeholder='Repetir Contraseña'
+              />
+            </div>
 
-          <button type='submit'>Registrar mi cuenta</button>
-        </form>
-        <p>
-          Al registrarse, usted acepta nuestros Términos y
-          Condiciones.
-        </p>
+            <button type='submit'>Registrar mi cuenta</button>
+          </form>
+          <p>
+            Al registrarse, usted acepta nuestros Términos y
+            Condiciones.
+          </p>
+        </div>
       </div>
     </div>
   );
