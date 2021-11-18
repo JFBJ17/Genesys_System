97c7045a6387ec2dd343ee6863734a49e07cb324
Dayanna Villanueva <dayannavillanueva452@gmail.com>
2021-11-18 06:22:30 -0500

    carritoCompras

---
+++ frontend/src/pages/ShoppingBag.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShoppingCard from '../components/ShoppingCard'
+
+const ShoppingBag = () => {
+    return (
+        <div>
+            <ShoppingCard/>
+        </div>
+    )
+}
+
+export default ShoppingBag
